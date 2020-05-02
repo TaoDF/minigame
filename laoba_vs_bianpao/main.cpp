16423499b27e640f4c3aae155b6d2223ad68acba
Yongren Yang <taodafen@Yongrens-MacBook-Air.local>
2020-05-02 04:24:53 -0400

    123

--- laoba_vs_bianpao/main.cpp
+++ laoba_vs_bianpao/main.cpp
@@ -5,6 +5,8 @@ using namespace sf;
 const float SCALE = 30.f;
 const float DEG = 57.29577f;
 
+enum whatever{START, BIANPAO, LAOBA, END};
+
 b2Vec2 Gravity(0.f, 9.8f);
 b2World World(Gravity);
 
@@ -25,13 +27,15 @@ int main()
     RenderWindow window(VideoMode(800,600), "Lao8 Volleyball");
     window.setFramerateLimit(60);
 
-    Texture t_bg, t_bianpao, t_lao8;
+    Texture t_bg, t_bianpao, t_lao8, t_kucha;
     t_bg.loadFromFile("./pic/bg.png");
     t_bianpao.loadFromFile("./pic/bianpao.png");
     t_lao8.loadFromFile("./pic/lao8.png");
+    t_kucha.loadFromFile("./pic/kucha.png");
 
-    Sprite sBackground(t_bg), sBianPao(t_bianpao), sLao8(t_lao8);
+    Sprite sBackground(t_bg), sBianPao(t_bianpao), sLao8(t_lao8), sKucha(t_kucha);
     sBianPao.setOrigin(32,32);
+    sKucha.setPosition(600,300);
 
     //---box2d-------
     setWall(400,550,2000,10); //floor
@@ -45,7 +49,7 @@ int main()
     b2BodyDef bdef;
     bdef.type = b2_dynamicBody;
     bdef.position.Set(10,1);
-    b2Body *b = World.CreateBody(&bdef);
+    b2Body *pB_bianpao = World.CreateBody(&bdef);
 
     b2CircleShape circle;
     circle.m_radius = 32/SCALE;
@@ -53,22 +57,22 @@ int main()
     fdef.shape = &circle;
     fdef.restitution = 0.95;
     fdef.density = 0.2;
-    b->CreateFixture(&fdef);
-    int data = 1;
-    b->SetUserData((void*)data);
+    pB_bianpao->CreateFixture(&fdef);
+    int data_bianpao = BIANPAO;
+    pB_bianpao->SetUserData((void*)data_bianpao);
     ////////players
     bdef.position.Set(20,2);
     b2CircleShape c;
     c.m_radius = 32/SCALE;
     c.m_p.Set(0,13/SCALE);
-    b2Body *pBody = World.CreateBody(&bdef);
-    pBody->CreateFixture(&c,5);
+    b2Body *pB_laoba = World.CreateBody(&bdef);
+    pB_laoba->CreateFixture(&c,5);
     c.m_radius = 25/SCALE;
     c.m_p.Set(0,-20/SCALE);
-    pBody->CreateFixture(&c,5);
-    pBody->SetFixedRotation(true);
-    int data_2 = 2;
-    pBody->SetUserData((void*)data_2);
+    pB_laoba->CreateFixture(&c,5);
+    pB_laoba->SetFixedRotation(true);
+    int data_laoba = LAOBA;
+    pB_laoba->SetUserData((void*)data_laoba);
 
     while(window.isOpen())
     {
@@ -83,8 +87,8 @@ int main()
 
         World.Step(1/60.f, 8, 3);
 
-        b2Vec2 pos = pBody->GetPosition();
-        b2Vec2 vel = pBody->GetLinearVelocity();
+        b2Vec2 pos = pB_laoba->GetPosition();
+        b2Vec2 vel = pB_laoba->GetLinearVelocity();
 
         if(Keyboard::isKeyPressed(Keyboard::Right)) vel.x = 5;
         if(Keyboard::isKeyPressed(Keyboard::Left)) vel.x = -5;
@@ -102,24 +106,26 @@ int main()
         if(!Keyboard::isKeyPressed(Keyboard::Down))
             vel.y = 0;
 
-        pBody->SetLinearVelocity(vel);
+        pB_laoba->SetLinearVelocity(vel);
 
         ///////Draw/////////
         window.draw(sBackground);
+        window.draw(sKucha);
+
 
         for(b2Body* it = World.GetBodyList(); it!=0; it = it->GetNext())
         {
             b2Vec2 pos = it->GetPosition();
             float angle = it->GetAngle();
 
-            if((uintptr_t)it->GetUserData()==2)
+            if((uintptr_t)it->GetUserData()==LAOBA)
             {
                 sLao8.setPosition(pos.x*SCALE, pos.y*SCALE);
                 sLao8.setRotation(angle*DEG);
                 window.draw(sLao8);
             }
 
-            if((uintptr_t)it->GetUserData()==1)
+            if((uintptr_t)it->GetUserData()==BIANPAO)
             {
                 sBianPao.setPosition(pos.x*SCALE, pos.y*SCALE);
                 sBianPao.setRotation(angle*DEG);
@@ -128,9 +134,6 @@ int main()
         }
         window.display();
     }
-
-
-
 
     return 0;
 }
